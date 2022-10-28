1284709b29463839ad92465a8860b494b4b06f3e
BillyLaBufanda307 <115328442+BillyLaBufanda307@users.noreply.github.com>
2022-10-28 19:46:08 -0400

    Add files via upload

--- GUI_root.py
+++ GUI_root.py
@@ -1,21 +1,32 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
 
 #Establish base for GUI as variable root
-root = tk.Tk()
-root.geometry("1000x1000")
+root = Tk()
+root.geometry("1000x500")
 root.title("GPRFE")
 
 #created basic heading named Datframe as variable heading 
-heading = tk.Label(root, text="Dataframe",borderwidth=0.5, relief="ridge",  font=('Arial', 18))
-heading.place(x=10, y=50, width = 200, height= 50)
+heading = Label(root, text="Dataframe",borderwidth=0.5, relief="ridge",  font=('Arial', 10))
+heading.pack(side='top', anchor='nw')
 
-#create basic query entry box where SQL scripts could extract data from??\
-#qbH = tk.Label(root, text="QUERY",borderwidth=0.5, relief="raised",  font=('Arial', 10))
-#qbH.pack(ipady=10, anchor=tk.NE)
-#querybox = tk.Entry(qbH, borderwidth=0.5, relief="sunken")#replace qbH???
-#querybox.pack()
-#querybox.pack(ipady=10, anchor=tk.NE)
+#Open file explorer script
+def openfile():
+    filename= filedialog.askopenfilenames(initialdir="/", title="Select Files", filetypes= ())
+    #create location to indicate opened file
+    fileindicatorframe=LabelFrame(root, padx=10, pady=10) #create frame to keep things organized
+    fileindicatorframe.pack(side='top', anchor='se') #set location of frame WIP
+    fileindicator= Label(root, borderwidth= 5, relief="sunken", width=100, text=filename) #create fileindicator label as variabel "fileindicator"
+    fileindicator.pack() #pack label into fileindicatorframe
+    return
 
-print("hello world")
+#create basic button to import file
+importframe = LabelFrame(root, padx=10, pady=10) #create frame to keep things organized
+importframe.pack(side='top', anchor='ne', padx=10)#set location of frame
+fileimport = Button(importframe, text= "Import", padx=10, pady=10, command=openfile) 
+#create button for importing files as variable "fileimport"
+#command=openfile calls open file script
+fileimport.pack() #pack button into importframe
+
 
 root.mainloop()
