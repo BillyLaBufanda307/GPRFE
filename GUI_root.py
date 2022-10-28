b99570f6143d0f8cdd880d71233cca67f13a7a5f
BillyLaBufanda307 <115328442+BillyLaBufanda307@users.noreply.github.com>
2022-10-28 13:44:25 -0400

    Add files via upload

--- GUI_root.py
+++ GUI_root.py
@@ -12,8 +12,10 @@ heading.place(x=10, y=50, width = 200, height= 50)
 #create basic query entry box where SQL scripts could extract data from??\
 #qbH = tk.Label(root, text="QUERY",borderwidth=0.5, relief="raised",  font=('Arial', 10))
 #qbH.pack(ipady=10, anchor=tk.NE)
-querybox = tk.Entry(qbH, borderwidth=0.5, relief="sunken")#replace qbH???
-querybox.pack()
+#querybox = tk.Entry(qbH, borderwidth=0.5, relief="sunken")#replace qbH???
+#querybox.pack()
 #querybox.pack(ipady=10, anchor=tk.NE)
+
+print("hello world")
 
 root.mainloop()
